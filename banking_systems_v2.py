be37bc07bc2f193dac7e21437ee60473f69ff1cf
Jairo Molina <jromolina@gmail.com>
2024-05-31 10:59:18 -0300

    Improved final modeling and improved code

--- banking_systems_v2.py
+++ banking_systems_v2.py
@@ -17,9 +17,9 @@ def depositar(saldo, valor, extrato, /):
         if valor > 0:
             saldo += valor
             extrato += f"Depósito:\tR$ {valor:.2f}\n"
-            print("\n=== Depósito realizado com sucesso! ===")
+            print("\n=#=#= Depósito realizado com sucesso! =#=#=")
         else:
-            print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+            print("\n@&@&@ Operação falhou! O valor informado é inválido. @&@$@")
 
         return saldo, extrato
 
@@ -29,22 +29,22 @@ def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
     excedeu_saques = numero_saques >= limite_saques
 
     if excedeu_saldo:
-        print("\n@@@ Operação falhou! Você não tem saldo suficiente. @@@")
+        print("\n@&@&@ Operação falhou! Você não tem saldo suficiente. @&@&@")
 
     elif excedeu_limite:
-        print("\n@@@ Operação falhou! O valor do saque excede o limite. @@@")
+        print("\n@&@&@ Operação falhou! O valor do saque excede o limite. @&@&@")
 
     elif excedeu_saques:
-        print("\n@@@ Operação falhou! Número máximo de saques excedido. @@@")
+        print("\n@&@&@ Operação falhou! Número máximo de saques excedido. @&@&@")
 
     elif valor > 0:
         saldo -= valor
         extrato += f"Saque:\t\tR$ {valor:.2f}\n"
         numero_saques += 1
-        print("\n=== Saque realizado com sucesso! ===")
+        print("\n=#=#= Saque realizado com sucesso! =#=#=")
 
     else:
-        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+        print("\n@&@&@ Operação falhou! O valor informado é inválido. @&@&@")
 
     return saldo, extrato
             
@@ -62,7 +62,7 @@ def criar_usuario(usuarios):
     usuario = filtrar_usuario(cpf, usuarios)
 
     if usuario:
-        print("\n@@@ Já existe usuário com esse CPF! @@@")
+        print("\n@&@&@ Já existe usuário com esse CPF! @&@&@")
         return
 
     nome = input("Informe o nome completo: ")
